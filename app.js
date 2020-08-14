2c1c3f9f5c30793c0d7a8e09b9cda35453833b7d
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2020-08-14 12:59:44 +0100

    Update: added forms to login and sign up pages

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Application is listening to PORT: ${PORT}`);
